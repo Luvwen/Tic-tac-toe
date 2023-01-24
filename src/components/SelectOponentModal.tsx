297057b51e4bcb9f075914d620bafcad83bed916
Santiago <santiagobalino@hotmail.com>
2023-01-24 02:14:56 -0300

    Add tablet size support

--- src/components/SelectOponentModal.tsx
+++ src/components/SelectOponentModal.tsx
@@ -20,38 +20,66 @@ export const SelectOponentModal = ({
             borderRadius="24px"
             display="flex"
             flexDirection={['column', 'column']}
-            height={openInstructionsModal ? ['80vh', '30vh'] : ['25vh', '20vh']}
+            height={
+                openInstructionsModal
+                    ? ['50vh', '70vh', '55vh', '50vh', '35vh']
+                    : ['25vh', '25vh', '23vh', '20vh']
+            }
             justifyContent={['space-evenly', 'space-evenly']}
-            pl="15px"
+            pl={['0', '0', '0', '0', '15px']}
             position="absolute"
-            top={openInstructionsModal ? ['12%', '35%'] : ['35%']}
-            width={openInstructionsModal ? ['90vw', '55vw'] : ['80vw', '30vw']}
+            top={
+                openInstructionsModal
+                    ? ['22%', '15%', '20%', '20%', '35%']
+                    : ['35%']
+            }
+            width={
+                openInstructionsModal
+                    ? ['90vw', '75vw', '65vw']
+                    : ['85vw', '75vw', '50vw', '40vw', '35vw']
+            }
             zIndex="15"
         >
             {openInstructionsModal && (
                 <Stack>
-                    <UnorderedList pr="15px" spacing="2">
-                        <ListItem fontSize="xl" fontWeight="bold">
+                    <UnorderedList pl="15px" spacing="2">
+                        <ListItem
+                            fontSize={['md', 'xl', 'xl']}
+                            fontWeight="bold"
+                        >
                             Players must take turns, making only 1 mark with
                             each turn.
                         </ListItem>
-                        <ListItem fontSize="xl" fontWeight="bold">
+                        <ListItem
+                            fontSize={['md', 'xl', 'xl']}
+                            fontWeight="bold"
+                        >
                             Marks can only be placed in empty squares, and once
                             it is placed, it is permanent.
                         </ListItem>
-                        <ListItem fontSize="xl" fontWeight="bold">
+                        <ListItem
+                            fontSize={['md', 'xl', 'xl']}
+                            fontWeight="bold"
+                        >
                             The winner is the first player to get 3 of their
                             marks in a straight line (the line can be positioned
                             diagonally, vertically, or horizontally).
                         </ListItem>
-                        <ListItem fontSize="xl" fontWeight="bold">
+                        <ListItem
+                            fontSize={['md', 'xl', 'xl']}
+                            fontWeight="bold"
+                        >
                             If neither player has a straight line of 3 marks, it
                             is considered a tie.
                         </ListItem>
                     </UnorderedList>
                 </Stack>
             )}
-            <Stack direction={['row']} spacing="7">
+            <Stack
+                direction={['row']}
+                pb={['15px', '10px', '0', '0', '0']}
+                spacing="7"
+            >
                 <Button
                     _hover={{
                         bg: 'hover',
